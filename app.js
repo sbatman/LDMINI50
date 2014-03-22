748afe0b0d2e08c2f198fa55e72bb665440c5ca0
sbatman <steven@insanedev.co.uk>
2014-03-22 11:38:51 +0000

    Working Game Canvas

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-﻿class MainGame
-{
-    game: Phaser.Game;
-
-    constructor()
-    {
+﻿var MainGame = (function () {
+    function MainGame() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
     }
-    preload() {
+    MainGame.prototype.preload = function () {
         this.game.load.image("TestImage", "Content/Graphics/TestImage.png");
-    }
-    create() {
+    };
+    MainGame.prototype.create = function () {
         this.game.stage.backgroundColor = "#ffffff";
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "TestImage");
         logo.anchor.setTo(0.5, 0.5);
-    }
-}
+    };
+    return MainGame;
+})();
 
-
-
-window.onload = () => {
+window.onload = function () {
     var instanceOfGame = new MainGame();
 };
+//# sourceMappingURL=app.js.map
